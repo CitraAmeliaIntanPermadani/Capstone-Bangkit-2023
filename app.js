fa42525781dc3b542f367bffeea1b31d954db5bb
sfrosianah <sfrosianah23@gmail.com>
2023-12-18 10:58:44 +0700

    fix get all stories

--- app.js
+++ app.js
@@ -10,23 +10,23 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Route ...
-app.get('/', (req,res) => {
+app.get('/v1/', (req,res) => {
     res.send('It is US, MicroBizMate - new dev')
 })
 // 1. auth
-app.use('/v1/auth', require('./src/routes/auth'))
+app.use('/auth', require('./src/routes/auth'))
 // 2. Dashboard
-app.use('/v1/dashboard', require('./src/routes/dashboard'))
+app.use('/dashboard', require('./src/routes/dashboard'))
 // 3. addStory
 const storiesRouter = require('./src/routes/addStories') // Make sure the path is correct
-app.use('/v1/stories', storiesRouter);
+app.use('/stories', storiesRouter);
 // 4. guestAccount
 const guestStoriesRouter = require('./src/routes/guestStories');
-app.use('/v1/stories/guest', guestStoriesRouter);
+app.use('/stories/guest', guestStoriesRouter);
 // 5. getAllStories
-app.use('/v1/stories', require('./src/routes/getAllStories'));
+app.use('/stories', require('./src/routes/getAllStories'));
 // 6. getDetailStories
-app.use('/v1/stories', require('./src/routes/getDetailStories'));
+app.use('/stories', require('./src/routes/getDetailStories'));
 
 // Jalanin Server
 const port = 8080
