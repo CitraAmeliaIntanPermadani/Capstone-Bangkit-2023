23a39ca194c77268d1b6771b2325a0ee4135cc68
sfrosianah <sfrosianah23@gmail.com>
2023-12-20 10:08:34 +0700

    fix app.js

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ app.get('/', (req,res) => {
     res.send('It is US, MicroBizMate - new dev')
 })
 // 1. auth
-app.use('/v1/auth/', require('./src/routes/auth'))
+app.use('/v1/auth', require('./src/routes/auth'))
 // 2. Dashboard
-app.use('/v1/dashboard/', require('./src/routes/dashboard'))
+app.use('/v1/dashboard', require('./src/routes/dashboard'))
 // 3. addStory
 const storiesRouter = require('./src/routes/addStories') // Make sure the path is correct
-app.use('/v1/stories/', storiesRouter);
+app.use('/v1/stories', storiesRouter);
 // 4. guestAccount
 const guestStoriesRouter = require('./src/routes/guestStories');
-app.use('/v1/stories/guest/', guestStoriesRouter);
+app.use('/v1/stories/guest', guestStoriesRouter);
 // 5. getAllStories
-app.use('/v1/stories/', require('./src/routes/getAllStories'));
+app.use('/v1/stories', require('./src/routes/getAllStories'));
 // 6. getDetailStories
-app.use('/v1/stories/', require('./src/routes/getDetailStories'));
+app.use('/v1/stories', require('./src/routes/getDetailStories'));
 
 
 
